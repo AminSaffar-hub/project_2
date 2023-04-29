a17ad1949cf547c6101ad927fbbffcaec51628bd
Amin SAFFAR <amin.saffar@lmad.eu>
2023-04-29 18:26:13 +0200

    create ha file

---
+++ scrapping/scrapping_hammadi_abid.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from tqdm import tqdm
+import pandas as pd
+import time
+import re 
