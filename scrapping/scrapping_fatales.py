f26dd56f378d259227c475493cdc3e88663f58f6
Amin SAFFAR <amin.saffar@lmad.eu>
2023-05-03 22:58:40 +0200

    add fatales scrapping code

---
+++ scrapping/scrapping_fatales.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+pre_process_prices = lambda a: float(a.replace("TND", "").replace(",", "."))
+class Scrapping:
+    def __init__(self):
+        self.urls = "https://www.fatales.tn/promotions"
+        self.html_file = "output.html"
+        self.timer = 10
+        self.name = []
+        self.image_link = []
+        self.product_type = []
+        self.product_description = []
+        self.old_price = []
+        self.new_price = []
+        self.url = []
+        
+    def get_product_description(self, product_url, driver):
+        driver.get(product_url)
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, "html.parser")
+        self.product_description.append(soup.find("div", itemprop= "description").text)
+
+    def main(self):
+        driver = webdriver.Chrome()
+        driver.get(self.urls)
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, "html.parser")
+        page_indexes = soup.find_all("a", class_="js-search-link")
+        number_of_pages = 0
+        for page in page_indexes:
+            try:
+                page_index = int(page.text)
+                if page_index > number_of_pages:
+                    number_of_pages = page_index
+            except Exception:
+                pass
+        for i in range(1, number_of_pages + 1):
+            driver.get(self.urls + f"?page={i}")
+            html_content = driver.page_source
+            soup = BeautifulSoup(html_content, "html.parser")
+            for article in soup.find_all("article", class_="product-miniature"):
+                self.image_link.append(article.find("img", class_="img-responsive")["src"])
+                self.name.append(article.find("a", class_="product-name").text)
+                try:
+                    self.old_price.append(pre_process_prices(article.find("span", class_="regular-price").text))
+                except Exception:
+                    self.old_price = None
+                self.new_price.append(pre_process_prices(article.find("span", class_="price product-price").text))
+                self.get_product_description(article.find("a", class_="product_img_link")['href'], driver=driver)
+
+
+if __name__ == "__main__":
+    scrapper = Scrapping()
+    scrapper.main()
