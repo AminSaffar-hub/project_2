ecb2360109d1975e54f1d31a56335b9284f9b0b0
Amin SAFFAR <amin.saffar@lmad.eu>
2023-05-14 21:12:25 +0200

    dockerize all elements of production server

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,14 +1,17 @@
 from backend.settings.base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    "192.168.1.13",
     "localhost",
     "host.docker.internal",
     "ec2-52-47-128-70.eu-west-3.compute.amazonaws.com",
     "13.38.188.241",
 ]
+
+STATIC_ROOT = "/www/static/"
+STATIC_URL = "/static/"
 
 DATABASES = {
     'default': {
