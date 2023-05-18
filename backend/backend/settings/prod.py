8f35e65727e9901878d379342c66497326dffa79
Amin Saffar <modamine@gmail.com>
2023-05-18 13:17:47 +0200

    finalize docker setup

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,9 +1,10 @@
 from backend.settings.base import *
+import os
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "192.168.1.13",
+    "192.168.1.56",
     "localhost",
     "host.docker.internal",
     "ec2-52-47-128-70.eu-west-3.compute.amazonaws.com",
@@ -16,9 +17,9 @@ STATIC_URL = "/static/"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
+        'NAME': os.getenv('POSTGRES_USER'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': 5400,
     }
