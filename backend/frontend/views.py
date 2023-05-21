73607fdb7080d0795327d20a843af363004014de
Amin SAFFAR <amin.saffar@lmad.eu>
2023-05-21 18:20:53 +0200

    add search bar

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -3,5 +3,9 @@ from backend.models import Article
 
 # Create your views here.
 def home(request):
-    articles = Article.objects.all()
+    search_post = request.GET.get('search')
+    if search_post:
+        articles = Article.objects.filter(name__icontains=search_post)
+    else:
+        articles = Article.objects.all()
     return render(request, 'frontend/home.html', {"articles": articles})
