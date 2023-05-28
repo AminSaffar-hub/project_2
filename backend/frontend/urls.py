2bcdd5a02991db7c4b901131ded1e024f6f47c20
Amin SAFFAR <amin.saffar@lmad.eu>
2023-05-28 19:25:58 +0200

    fix pagination on searched articles

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("<int:page>", views.home, name="pages"),
 ]
--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -4,21 +4,24 @@ from backend.models import Article
 
 
 # Create your views here.
-def home(request, page=1):
-    search_post = request.GET.get("search")
-    if search_post:
-        articles = Article.objects.filter(name__icontains=search_post).order_by("name")
+def home(request):
+    searched_article = request.GET.get("search")
+    page_number = request.GET.get("page") or 1
+    if searched_article:
+        articles = Article.objects.filter(name__icontains=searched_article).order_by(
+            "name"
+        )
     else:
         articles = Article.objects.all().order_by("name")
 
     paginator = Paginator(articles, per_page=8)
-    articles_in_page = paginator.get_page(page)
+    articles_in_page = paginator.get_page(page_number)
     articles_in_page.adjusted_elided_pages = paginator.get_elided_page_range(
-        page, on_each_side=2, on_ends=2
+        page_number, on_each_side=2, on_ends=2
     )
 
     return render(
         request,
         "frontend/home.html",
-        {"articles": articles_in_page, "search_post": search_post},
+        {"articles": articles_in_page, "searched_article": searched_article},
     )
