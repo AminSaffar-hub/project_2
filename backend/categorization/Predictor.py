8f912338678aa3770e92f466e8d0d27e07119bd3
med-tz <med.touzi1996@gmail.com>
2024-03-20 20:23:32 +0100

    added predictor class

--- backend/categorization/Predictor.py
+++ backend/categorization/Predictor.py
@@ -3,15 +3,18 @@ from transformers import AutoTokenizer
 import numpy as np
 import json
 
+
 class Predictor:
     def __init__(self, path, categories_json_path):
         self.model = ORTModelForSequenceClassification.from_pretrained(path)
         self.tokenizer = AutoTokenizer.from_pretrained(path)
-        with open(categories_json_path, 'r') as f:
+        with open(categories_json_path, "r") as f:
             self.idx_to_category = json.load(f)
 
     def predict_category(self, text):
-        inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+        inputs = self.tokenizer(
+            text, return_tensors="pt", truncation=True, padding=True
+        )
         inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
         outputs = self.model(**inputs)
         predictions = outputs.logits
